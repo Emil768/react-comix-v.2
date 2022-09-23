f9ca7fcd25281960c94d771726d4b53b3cc23948
EmilMurahas228 <murakhas@list.ru>
2022-09-23 20:04:37 +0300

    feat: add footer

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -2,6 +2,7 @@ export { default as CartItem } from "./CartItem/index";
 export { default as Categories } from "./Categories";
 export { default as ComixBlock } from "./ComixBlock";
 export { default as Header } from "./Header";
+export { default as Footer } from "./Footer";
 export { default as LoadingBlock } from "./LoadingBlock";
 export { default as SortPopup } from "./SortPopup";
 export { default as Search } from "./Search";
