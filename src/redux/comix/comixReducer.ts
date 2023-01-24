5ae68f6f5e2032d32b5d64a30cbcd3a56de62aa8
EmilMurahas228 <murakhas@list.ru>
2023-01-24 13:38:27 +0300

    Change: add new axios url

--- src/redux/comix/comixReducer.ts
+++ src/redux/comix/comixReducer.ts
@@ -1,5 +1,5 @@
 import { createSlice, createAsyncThunk, PayloadAction } from "@reduxjs/toolkit";
-import axios from "axios";
+import axios from "../../utils/axios";
 
 import { ComixState, ComixTotal, FilterProps } from "./types";
 
